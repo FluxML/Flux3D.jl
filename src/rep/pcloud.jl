ad26fef80b90a9874b3559b4ba4662a13c37dd1c
nirmal <nirmalps@iitk.ac.in>
2020-04-28 02:11:01 +0530

    Change PointCloud representation to include normals

--- src/rep/pcloud.jl
+++ src/rep/pcloud.jl
@@ -1,15 +1,62 @@
-export PointCloud
+export PointCloud, npoints
 
-struct PointCloud <: AbstractObject
+"""
+    PointCloud
+
+Initialize PointCloud representation.
+
+`points` should be Array of size `(N,D)` where `N` is the number of points and
+`D` is dimensionality of each points (i.e. `D`=2 or `D`=3). `normals` is optional
+field, if given should be Array of size `(N,D)` where `N` should match with the
+`N` of `points` and `D`=2 or `D`=3 (i.e. normals for 2D and 3D PointCloud respectively).
+
+### Feilds:
+
+- `points`      - Points that makes up whole PointCloud. 
+- `normals`     - Normals of each points in PointCloud.
+
+### Available Contructor:
+
+- `PointCloud(points::Array{T,2}, normals::Union(Array{R,2}, nothing)=nothing) where {T<:Number,R<:Number}`
+- `PointCloud(;points, normals=nothing)`
+- `PointCloud(pcloud::PointCloud)`
+"""
+mutable struct PointCloud <: AbstractObject
     points::Array{Float32,2}
-    npoints::Int
+    normals::Union{Array{Float32,2}, Nothing}
 end
 
-function PointCloud(points::Array{T,2} where{T<:Number})
-    points =convert(Array{Float32,2},points)
-    PointCloud(points, size(points,1))
+function PointCloud(points::Array{T,2}, normals::Union{Array{R,2}, Nothing}=nothing) where {T<:Number,R<:Number}
+    points = convert(Array{Float32,2},points)
+
+    if normals != nothing
+        size(points,1) == size(normals,1) || error("number of points and normals must match in PointCloud.")
+        normals = convert(Array{Float32,2},normals);
+    end
+
+    PointCloud(points, normals)
 end
 
-PointCloud(points::Array{Float32,2}) = PointCloud(points, size(points,1))
+PointCloud(;points, normals=nothing)= PointCloud(points, normals)
 
-PointCloud(;points) = PointCloud(points)
+PointCloud(pcloud::PointCloud) = PointCloud(pcloud.points, pcloud.normals)
+
+Base.getindex(p::PointCloud, I...) = p.points[I...]
+
+function Base.show(io::IO, p::PointCloud)
+    if p.normals isa Nothing
+        print(io, "points: $(size(p.points)), normals: nothing")
+    else
+        print(io, "points: $(size(p.points)), normals: $(size(p.normals))")
+    end
+end
+
+Base.show(io::IO, ::MIME"text/plain", p::PointCloud) = 
+    print(io, "PointCloud object:\n   ", p)
+
+"""
+    npoints(p::PointCloud)
+
+Returns the size of PointCloud.
+"""
+npoints(p::PointCloud) = size(p.points,1)
--- src/visualize.jl
+++ src/visualize.jl
@@ -1,15 +1,24 @@
 export visualize
 
+"""
+    visualize(pcloud::PointCloud; kwargs...)
+
+Visualize PointCloud `pcloud`.
+
+Dimension of points in PointCloud `pcloud` must be 3.
+
+### Optional Arguments:
+- color (Symbol)        - Color of the marker, default `:blue`
+- markersize (Number)   - Size of the marker, default `0.02*npoints(pcloud)/1024`  
+"""
 function visualize(v::PointCloud; kwargs...)
-    # get!(kwargs, :markersize) do
-    #     0.02*v.npoints/1024
-    # end
-    # get!(kwargs, :color) do
-    #     :blue
-    # end
-    color = get(kwargs, :color, :blue)
-    markersize = get(kwargs, :markersize, 0.02*v.npoints/1024)
-    Makie.AbstractPlotting.meshscatter(v.points[:,1],v.points[:,2],v.points[:,3], color=color, markersize=markersize)
+    size(v.points,2)==3 || error("dimension of points in PointCloud must be 3.")
+
+    kwargs = convert(Dict{Symbol, Any}, kwargs)
+    get!(kwargs, :color, :blue)
+    get!(kwargs, :markersize, 0.02*npoints(v)/1024)
+
+    Makie.AbstractPlotting.meshscatter(v.points[:,1],v.points[:,2],v.points[:,3]; kwargs...)
 end
 
 visualize(v::AbstractDataPoint; kwargs...) = visualize(v.data; kwargs...)
