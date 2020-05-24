d56d8c0560e5ac99f9e5b8f6e723556b64781c88
nirmal <nirmalps@iitk.ac.in>
2020-05-24 19:09:46 +0530

    Fix examples

--- examples/dgcnn_classification.jl
+++ examples/dgcnn_classification.jl
@@ -25,9 +25,9 @@ end
 function get_processed_data(args)
     # Fetching the train and validation data and getting them into proper shape	
     if args.num_classes == 10
-        dset = ModelNet10.dataset(;mode=pointcloud, npoints=args.npoints, transforms=NormalizePointCloud())
+        dset = ModelNet10.dataset(;mode=:pointcloud, npoints=args.npoints, transform=NormalizePointCloud())
     elseif args.num_classes == 40
-        dset = ModelNet40.dataset(;mode=pointcloud, npoints=args.npoints, transforms=NormalizePointCloud())
+        dset = ModelNet40.dataset(;mode=:pointcloud, npoints=args.npoints, transform=NormalizePointCloud())
     else
         error("ModelNet dataset with $(args.num_classes) is not supported.
                 Currently supported num_classes for ModelNet dataset is {10,40}")
@@ -37,19 +37,19 @@ function get_processed_data(args)
     labels = onehotbatch([dset[i].ground_truth for i in 1:length(dset)],1:args.num_classes)
 
     #onehot encode labels of batch
-    train = [(cat(data[i]..., dims = 3), labels[:,i]) for i in partition(1:length(data), batch_size)] .|> args.device
+    train = [(cat(data[i]..., dims = 3), labels[:,i]) for i in partition(1:length(data), args.batch_size)] .|> args.device
     
     if args.num_classes == 10
-        VAL = ModelNet10.dataset(;mode=pointcloud, train=false, npoints=args.npoints, transforms=NormalizePointCloud())
+        VAL = ModelNet10.dataset(;mode=:pointcloud, train=false, npoints=args.npoints, transform=NormalizePointCloud())
     elseif args.num_classes == 40
-        VAL = ModelNet40.dataset(;mode=pointcloud, train=false, npoints=args.npoints, transforms=NormalizePointCloud())
+        VAL = ModelNet40.dataset(;mode=:pointcloud, train=false, npoints=args.npoints, transform=NormalizePointCloud())
     else
         error("ModelNet dataset with $(args.num_classes) is not supported.
                 Currently supported num_classes for ModelNet dataset is {10,40}")
     end
 
     valX = cat([VAL[i].data.points for i in 1:length(VAL)]..., dims=3) |> args.device
-    valY = onehotbatch([VAL[i].ground_truth for i in 1:length(VAL)], 1:num_classes) |> args.device
+    valY = onehotbatch([VAL[i].ground_truth for i in 1:length(VAL)], 1:args.num_classes) |> args.device
 
     val = (valX,valY)	
     return train, val
@@ -77,18 +77,18 @@ function train(; kwargs...)
     m = DGCNN(args.num_classes, args.K, args.npoints) |> args.device
 
     loss(x, y) = crossentropy(m(x), y)
-    accuracy(x, y) = mean(onecold(cpu(m(x)), 1:num_classes) .== onecold(cpu(y), 1:num_classes))
+    accuracy(x, y) = mean(onecold(cpu(m(x)), 1:args.num_classes) .== onecold(cpu(y), 1:args.num_classes))
 
     ## Training
     opt = ADAM(args.lr)
     @info("Training...")
     # Starting to train models
-    custom_train!(loss, params(m), train, opt, args.epochs, val)
+    custom_train!(loss, params(m), train, opt, args.epochs, val, accuracy)
 
     return m
 end
 
-function custom_train!(loss, ps, data, opt, epochs, (valX, valY))
+function custom_train!(loss, ps, data, opt, epochs, (valX, valY), accuracy)
     ps = Zygote.Params(ps)
     for epoch in 1:epochs
         running_loss = 0
