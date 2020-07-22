63aa2b2f56c323303f1eae054242a80ee5c7d476
nirmal <nirmalps@iitk.ac.in>
2020-07-23 02:54:09 +0530

    Fix tests

--- src/transforms/transforms.jl
+++ src/transforms/transforms.jl
@@ -1,4 +1,13 @@
-export ScalePointCloud, RotatePointCloud, ReAlignPointCloud, NormalizePointCloud
+export ScalePointCloud,
+    RotatePointCloud,
+    ReAlignPointCloud,
+    NormalizePointCloud,
+    ScaleTriMesh,
+    RotateTriMesh,
+    ReAlignTriMesh,
+    NormalizeTriMesh,
+    TranslateTriMesh,
+    OffsetTriMesh
 
 """
     ScalePointCloud(factor::Number; inplace::Bool=true)
@@ -25,12 +34,13 @@ end
 @functor ScalePointCloud
 
 function (t::ScalePointCloud)(pcloud::PointCloud)
-    t.inplace || (pcloud = deepcopy(pcloud);)
+    t.inplace || (pcloud = deepcopy(pcloud))
     scale!(pcloud, t.factor)
     return pcloud
 end
 
-Base.show(io::IO, t::ScalePointCloud) = print(io, "$(typeof(t))(factor=$(t.factor); inplace=$(t.inplace))")
+Base.show(io::IO, t::ScalePointCloud) =
+    print(io, "$(typeof(t))(factor=$(t.factor); inplace=$(t.inplace))")
 
 """
     RotatePointCloud(rotmat::AbstractArray{<:Number,2}; inplace::Bool=true)
@@ -50,19 +60,21 @@ struct RotatePointCloud <: AbstractTransform
 end
 
 function RotatePointCloud(rotmat::AbstractArray{<:Number,2}; inplace::Bool = true)
-    size(rotmat) == (3, 3) || error("rotmat must be (3,3) array, but instead got $(size(rotmat)) array")
+    size(rotmat) == (3, 3) ||
+        error("rotmat must be (3,3) array, but instead got $(size(rotmat)) array")
     return RotatePointCloud(Float32.(rotmat), inplace)
 end
 
 @functor RotatePointCloud
 
 function (t::RotatePointCloud)(pcloud::PointCloud)
-    t.inplace || (pcloud = deepcopy(pcloud);)
+    t.inplace || (pcloud = deepcopy(pcloud))
     rotate!(pcloud, t.rotmat)
     return pcloud
 end
 
-Base.show(io::IO, t::RotatePointCloud) = print(io, "$(typeof(t))(rotmat; inplace=$(t.inplace))")
+Base.show(io::IO, t::RotatePointCloud) =
+    print(io, "$(typeof(t))(rotmat; inplace=$(t.inplace))")
 
 """
     ReAlignPointCloud(target::PointCloud; inplace::Bool=true)
@@ -82,7 +94,7 @@ struct ReAlignPointCloud <: AbstractTransform
     inplace::Bool
 end
 
-function ReAlignPointCloud(target::PointCloud, index::Number=1; inplace::Bool = true)
+function ReAlignPointCloud(target::PointCloud, index::Number = 1; inplace::Bool = true)
     points = target[index]
     t_min = minimum(points, dims = 2)
     t_max = maximum(points, dims = 2)
@@ -90,17 +102,18 @@ function ReAlignPointCloud(target::PointCloud, index::Number=1; inplace::Bool = 
 end
 
 ReAlignPointCloud(target::AbstractArray{<:Number}; inplace::Bool = true) =
-    ReAlignPointCloud(PointCloud(target), inplace=inplace)
+    ReAlignPointCloud(PointCloud(target), inplace = inplace)
 
 @functor ReAlignPointCloud
 
 function (t::ReAlignPointCloud)(pcloud::PointCloud)
-    t.inplace || (pcloud = deepcopy(pcloud);)
+    t.inplace || (pcloud = deepcopy(pcloud))
     realign!(pcloud, t.t_min, t.t_max)
     return pcloud
 end
 
-Base.show(io::IO, t::ReAlignPointCloud) = print(io, "$(typeof(t))(target=PointCloud(...); inplace=$(t.inplace))")
+Base.show(io::IO, t::ReAlignPointCloud) =
+    print(io, "$(typeof(t))(target=PointCloud(...); inplace=$(t.inplace))")
 
 """
     NormalizePointCloud(; inplace::Bool=true)
@@ -121,7 +134,7 @@ NormalizePointCloud(; inplace::Bool = true) = NormalizePointCloud(inplace)
 @functor NormalizePointCloud
 
 function (t::NormalizePointCloud)(pcloud::PointCloud)
-    t.inplace || (pcloud = deepcopy(pcloud);)
+    t.inplace || (pcloud = deepcopy(pcloud))
     normalize!(pcloud)
     return pcloud
 end
@@ -154,12 +167,13 @@ end
 @functor ScaleTriMesh
 
 function (t::ScaleTriMesh)(m::TriMesh)
-    t.inplace || (m = deepcopy(m);)
+    t.inplace || (m = deepcopy(m))
     scale!(m, t.factor)
     return m
 end
 
-Base.show(io::IO, t::ScaleTriMesh) = print(io, "$(typeof(t))(factor=$(t.factor); inplace=$(t.inplace))")
+Base.show(io::IO, t::ScaleTriMesh) =
+    print(io, "$(typeof(t))(factor=$(t.factor); inplace=$(t.inplace))")
 
 """
     RotateTriMesh(rotmat::AbstractArray{<:Number,2}; inplace::Bool=true)
@@ -179,19 +193,21 @@ struct RotateTriMesh <: AbstractTransform
 end
 
 function RotateTriMesh(rotmat::AbstractArray{<:Number,2}; inplace::Bool = true)
-    size(rotmat) == (3, 3) || error("rotmat must be (3,3) array, but instead got $(size(rotmat)) array")
+    size(rotmat) == (3, 3) ||
+        error("rotmat must be (3,3) array, but instead got $(size(rotmat)) array")
     return RotateTriMesh(Float32.(rotmat), inplace)
 end
 
 @functor RotateTriMesh
 
 function (t::RotateTriMesh)(m::TriMesh)
-    t.inplace || (m = deepcopy(m);)
+    t.inplace || (m = deepcopy(m))
     rotate!(m, t.rotmat)
     return m
 end
 
-Base.show(io::IO, t::RotateTriMesh) = print(io, "$(typeof(t))(rotmat; inplace=$(t.inplace))")
+Base.show(io::IO, t::RotateTriMesh) =
+    print(io, "$(typeof(t))(rotmat; inplace=$(t.inplace))")
 
 """
     ReAlignTriMesh(target::TriMesh; inplace::Bool=true)
@@ -209,7 +225,7 @@ struct ReAlignTriMesh <: AbstractTransform
     inplace::Bool
 end
 
-function ReAlignTriMesh(target::TriMesh, index::Integer=1; inplace::Bool = true)
+function ReAlignTriMesh(target::TriMesh, index::Integer = 1; inplace::Bool = true)
     verts = get_verts_list(target)[index]
     t_min = minimum(verts, dims = 2)
     t_max = maximum(verts, dims = 2)
@@ -219,12 +235,13 @@ end
 @functor ReAlignTriMesh
 
 function (t::ReAlignTriMesh)(m::TriMesh)
-    t.inplace || (m = deepcopy(m);)
+    t.inplace || (m = deepcopy(m))
     realign!(m, t.t_min, t.t_max)
     return m
 end
 
-Base.show(io::IO, t::ReAlignTriMesh) = print(io, "$(typeof(t))(target=TriMesh(...); inplace=$(t.inplace))")
+Base.show(io::IO, t::ReAlignTriMesh) =
+    print(io, "$(typeof(t))(target=TriMesh(...); inplace=$(t.inplace))")
 
 """
     NormalizeTriMesh(; inplace::Bool=true)
@@ -245,7 +262,7 @@ NormalizeTriMesh(; inplace::Bool = true) = NormalizeTriMesh(inplace)
 @functor NormalizeTriMesh
 
 function (t::NormalizeTriMesh)(m::TriMesh)
-    t.inplace || (m = deepcopy(m);)
+    t.inplace || (m = deepcopy(m))
     normalize!(m)
     return m
 end
@@ -267,23 +284,25 @@ struct TranslateTriMesh <: AbstractTransform
     inplace::Bool
 end
 
-function TranslateTriMesh(vector::AbstractArray{<:Number, 1}; inplace::Bool = true)
-    (size(vector) == (3,)) || error("vector must be (3, ), but instead got $(size(vector)) array")
+function TranslateTriMesh(vector::AbstractArray{<:Number,1}; inplace::Bool = true)
+    (size(vector) == (3,)) ||
+        error("vector must be (3, ), but instead got $(size(vector)) array")
     TranslateTriMesh(Float32.(vector), inplace)
 end
 
 TranslateTriMesh(vector::Number; inplace::Bool = true) =
-    TranslateTriMesh(fill(vector, (3,)); inplace=inplace)
+    TranslateTriMesh(fill(vector, (3,)); inplace = inplace)
 
 @functor TranslateTriMesh
 
 function (t::TranslateTriMesh)(m::TriMesh)
-    t.inplace || (m = deepcopy(m);)
+    t.inplace || (m = deepcopy(m))
     translate!(m, t.vector)
     return m
 end
 
-Base.show(io::IO, t::TranslateTriMesh) = print(io, "$(typeof(t))(vector=$(t.vector);inplace=$(t.inplace))")
+Base.show(io::IO, t::TranslateTriMesh) =
+    print(io, "$(typeof(t))(vector=$(t.vector);inplace=$(t.inplace))")
 
 """
     OffsetTriMesh(offset_verts::AbstractArray{<:Number,2}; inplace::Bool=true)
@@ -296,7 +315,7 @@ If `inplace` is set to `false`, it will create deepcopy of TriMesh.
 See also: [`offset`](@ref), [`offset!`](@ref)
 """
 struct OffsetTriMesh <: AbstractTransform
-    offset_verts::AbstractArray{Float32, 2}
+    offset_verts::AbstractArray{Float32,2}
     inplace::Bool
 end
 
@@ -306,9 +325,10 @@ OffsetTriMesh(offset_verts::AbstractArray{<:Number,2}; inplace::Bool = true) =
 @functor OffsetTriMesh
 
 function (t::OffsetTriMesh)(m::TriMesh)
-    t.inplace || (m = deepcopy(m);)
+    t.inplace || (m = deepcopy(m))
     offset!(m, t.offset_verts)
     return m
 end
 
-Base.show(io::IO, t::OffsetTriMesh) = print(io, "$(typeof(t))(offset_verts; inplace=$(t.inplace))")
+Base.show(io::IO, t::OffsetTriMesh) =
+    print(io, "$(typeof(t))(offset_verts; inplace=$(t.inplace))")
